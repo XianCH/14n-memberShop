b08f9314d25766f311e4cea1a78c51b32ad35e7e
XianCH <1160698903@qq.com>
2023-12-19 21:15:24 +0800

    systemController

---
+++ app/controller/SystemController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\controller;
+
+class SystemController
+{
+    function downLoadApi()
+    {
+    }
+
+    function InOrder()
+    {
+    }
+
+    function payorder()
+    {
+    }
+
+    function getOnlineUser()
+    {
+    }
+
+    function getAllUserInSystem()
+    {
+    }
+
+    function getUserInfoByUuid()
+    {
+    }
+
+    function getSystemTranslate()
+    {
+    }
+
+    function getMusicLib()
+    {
+    }
+
+    function getMusciInfoByUUID()
+    {
+    }
+
+    function updataMusicInfo()
+    {
+    }
+}
+
