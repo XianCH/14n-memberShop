376c819166b19d45d46e11974c8b8b470b6aa3da
XianCH <1160698903@qq.com>
2023-12-19 21:15:08 +0800

    baseController

--- app/controller/BaseController.php
+++ app/controller/BaseController.php
@@ -1,37 +1,35 @@
 <?php
 
 namespace app\controller;
+
 use think\Response;
 use think\Validate;
 use think\facade\Request;
 
 
 
-class BaseController {
-    
-    protected function success($message = 'success',$code = 200,$data =[])
+class BaseController
+{
+
+    protected function success($message = 'success', $code = 200, $data = [])
     {
-        return $this->jsonResponse($data,$message,$code);
+        return $this->jsonResponse($data, $message, $code);
     }
 
-    protected function error($message = 'error' ,$code = 400,$data = [])
+    protected function error($message = 'error', $code = 400, $data = [])
     {
-        return $this->jsonResponse($data,$message,$code);
+        return $this->jsonResponse($data, $message, $code);
     }
 
 
-    private function jsonResponse($data,$message,$code)
+    private function jsonResponse($data, $message, $code)
     {
         $responseData = [
             'code' => $code,
             'message' => $message,
-            'data'=>$data,
+            'data' => $data,
         ];
-        return Response::create($responseData,'json',$code);
+        return Response::create($responseData, 'json', $code);
     }
-
-    // protected $rule = [
-    //     'name'  =>  'require|max:25',
-    //     'email' =>  'email',
-    // ];
 }
+
